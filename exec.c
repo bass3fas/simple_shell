e5f6fa3a2fc4cba95ed768967b34fb9649025d3b
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-09-20 00:50:56 -0700

    executing with full path only

---
+++ exec.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * exec - execute commands
+ * @words: commands array
+ */
+void exec(char **words)
+{
+	char *command = NULL, *real_command;
+
+	if (words != NULL)
+	{
+		command = words[0];
+		real_command = get_path(command);
+		if (execve(real_command, words, NULL) == -1)
+			perror("Error:");
+	}
+}
