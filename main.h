da97b88beff06a551787a1b7729181769fff3ccd
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-09-20 00:49:47 -0700

    header file with all libraries and functions prototypes

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+void exec(char **words);
+char *get_path( char *command);
+
+
+
+#endif
